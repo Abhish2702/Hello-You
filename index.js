e5293f1762863ee3af6aab4154c53ab085b612a7
Abhishek <abhishekbansal2002@gmail.com>
2022-01-16 15:48:12 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//First step is always to install express
+const express=require('express');
+const port=8000;
+const app=express();
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error: ${err}`);
+        return;
+    }
+    console.log(`Server is running at: ${port}`);
+});
