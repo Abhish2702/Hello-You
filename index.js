7f266e4f7aadd130047241173779a05e476ba1f3
Abhishek <abhishekbansal2002@gmail.com>
2022-01-18 19:08:52 +0530

    Added new controller and a new route

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const express=require('express');
 const port=8000;
 const app=express();
+
 app.use('/',require('./routes/index'));
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error: ${err}`);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const express=require('express');
 const router=express.Router();
 const homeController=require('../controllers/homeController');
+
 console.log('router called');
+
 router.get('/',homeController.home);
-router.get('/create',homeController.create);
+
+
+router.use('/users',require('./users'));
+
 module.exports=router;
