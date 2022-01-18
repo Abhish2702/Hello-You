0140a5f16b3f6c39cb0b1ab5423e3cc872fc0dfb
Abhishek <abhishekbansal2002@gmail.com>
2022-01-18 19:12:34 +0530

    Added views engine

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 const express=require('express');
 const port=8000;
 const app=express();
+//set express router
+app.use('/',require('./routes'));
 
-app.use('/',require('./routes/index'));
-
+//install ejs
+//setup views engine
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err){
         console.log(`Error: ${err}`);
